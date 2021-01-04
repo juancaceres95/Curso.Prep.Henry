c6d27b9bd5eca153782b915368a0cf333cd03447
juancaceres95 <caceres.juanignacio95@gmail.com>
2021-01-04 19:17:17 -0300

    JS04

---
+++ 04-JS-III/homework/array.md
@@ -0,0 +1,5 @@
+Array: un array o arreglo es un contenedor de almacenamiento de cualquier tipo de dato ( bool, numero, string o mismas funciones, etc). La sintaxis de un array es declarar una variable y establecerla dentro de corchetes [].
+Nosotros podemos acceder a los elementos de un arreglo, pero tenemos que tener en cuenta que el indice siempre arranca en 0.
+
+Los array cuentan con metedos para asignar datos como puede ser push y pop, tambien cuenta con metodos para eliminar elementos como son shift y unshift.
+Podemos iterar o recorrer un array con el bucle for utilizando el metodo .length como punto de partida del ciclo.
--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,29 +3,44 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un array
   // Tu código:
+  return array[0];
 }
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
-}
+    
+    return array[array.length -1];
+  }
+  
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+  return array.length;
 }
+
+
 
 function incrementarPorUno(array) {
   // "array" debe ser una matriz de enteros (int/integers)
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+  var newArray = [];
+  for(var i = 0; i < array.length; i++){
+    newArray[i] = array[i] +1;
+  }
+  return newArray;
 }
 
 function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+  
+  array.push(elemento);
+  return array;
 }
 
 function agregarItemAlComienzoDelArray(array, elemento) {
@@ -33,6 +48,8 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
+  array.unshift(elemento);
+  return array;
 }
 
 function dePalabrasAFrase(palabras) {
@@ -41,38 +58,67 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
+return palabras.join(' ');
 }
 
 function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
-}
+  for (var i = 0; i < array.length; i ++){
+    if(array[i]=== elemento){
+      return true;
+    }
+  }
+  return false;
+ }
 
 function agregarNumeros(numeros) {
   // "array" debe ser una matriz de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
+  var suma = 0;
+  for (var i = 0; i < numeros.length; i++){
+    suma = suma + numeros[i];
+  }
+  return suma;
 }
 
 function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
+  return agregarNumeros(resultadosTest) / resultadosTest.length;
+  
 }
+//console.log(promedioResultadosTest());
 
 function numeroMasGrande(numeros) {
-  // "numeros" debe ser una matriz de enteros (int/integers)
-  // Devuelve el número más grande
-  // Tu código:
+  //var numeros =[10, 20, 110, 300];
+  var maximo = numeros[0];
+  for(var i = 1; i < numeros.length; i++) {
+    if(numeros[i] > maximo) { 
+      maximo = numeros[i];
+    }
+  }
+  return maximo;
 }
+//console.log(numeroMasGrande());
 
 function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0
   // Si se pasa un argumento, simplemente devuélvelo
   // Tu código:
+  if(arguments.length < 1) return 0;
+  var total = 1;
+  for(var i = 0; i < arguments.length; i++) {
+    total = total * arguments[i];
+  }
+  return total;
 }
+console.log(multiplicarArgumentos(10, 20));
+
 
 // No modificar nada debajo de esta línea
 // --------------------------------
